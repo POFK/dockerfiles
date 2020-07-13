188a9d712fb163e66fb77c9b8422cf1a5e8ac13c
POFK <mtianxiang@gmail.com>
2020-07-13 22:38:43 +0800

    feat(vim): add tmux configuration to vim env

--- vim/entrypoint.sh
+++ vim/entrypoint.sh
@@ -3,6 +3,11 @@
 $HOME/git/dotfiles/dotdrop.sh -c $HOME/git/dotfiles/config.yaml install -p docker
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+proxychains4 ~/.fzf/install
+
+git clone --depth 1 https://github.com/samoshkin/tmux-config.git ~/tmux-config
+proxychains4 ~/tmux-config/install.sh
+
+mv ~/.tmux.conf.bak ~/.tmux.conf
 
 #exec $@
