70ab5da051500deaa5b3c6779eb80059842a21b7
POFK <mtianxiang@gmail.com>
2020-07-17 15:39:53 +0800

    build(vim): change entrypoint.sh

--- vim/entrypoint.sh
+++ vim/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
-$HOME/git/dotfiles/dotdrop.sh -c $HOME/git/dotfiles/config.yaml install -p docker
+yes | $HOME/git/dotfiles/dotdrop.sh -c $HOME/git/dotfiles/config.yaml install -p docker
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-proxychains4 ~/.fzf/install
+yes | proxychains4 ~/.fzf/install
 
 git clone --depth 1 https://github.com/samoshkin/tmux-config.git ~/tmux-config
-proxychains4 ~/tmux-config/install.sh
+~/tmux-config/install.sh
+
+yes | $HOME/git/dotfiles/dotdrop.sh -c $HOME/git/dotfiles/config.yaml install -p docker
 
 #exec $@
