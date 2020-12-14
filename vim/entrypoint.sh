d2db62330087580698645816715f2175b8f0e60b
POFK <mtianxiang@gmail.com>
2020-12-14 03:02:20 +0000

    update vim Dockerfile and entrypoint

--- vim/entrypoint.sh
+++ vim/entrypoint.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/bash
 
+sudo chown -R dev /opt/*
+
+curl http://git.storage.datalab/POFK/ansible-init/raw/main/files/add_caches.sh | sh -s pip
+
+curl http://git.storage.datalab/POFK/ansible-init/raw/main/files/add_caches.sh | sh -s conda
+
+yes | conda install ruamel ruamel.yaml
+
+yes | pip install -r $HOME/git/dotfiles/dotdrop/requirements.txt
+
 yes | $HOME/git/dotfiles/dotdrop.sh -c $HOME/git/dotfiles/config.yaml install -p docker
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-yes | proxychains4 ~/.fzf/install
+yes | ~/.fzf/install
 
 git clone --depth 1 https://github.com/samoshkin/tmux-config.git ~/tmux-config
 ~/tmux-config/install.sh
